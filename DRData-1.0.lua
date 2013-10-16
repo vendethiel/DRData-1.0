3e70cc2f444fd6d1a6a886c7b6461ffdb442648c
Adirelle <adirelle@gmail.com>
2013-10-16 14:37:15 +0200

    Adding translations using wowace localization system.
    See http://www.wowace.com/addons/drdata-1-0/localization/.

--- DRData-1.0.lua
+++ DRData-1.0.lua
@@ -1,5 +1,5 @@
 local major = "DRData-1.0"
-local minor = 1025
+local minor = 1026
 assert(LibStub, string.format("%s requires LibStub.", major))
 
 local Data = LibStub:NewLibrary(major, minor)
@@ -22,21 +22,27 @@ local L = {
 	["Knockbacks"] = "Knockbacks",
 }
 
-if GetLocale() == "frFR" then
-	L["Roots"] = "Immobilisations"
-	L["Stuns"] = "Etourdissements"
-	L["Cyclone"] = "Cyclone"
-	L["Disarms"] = "Désarmements"
-	L["Mesmerizes"] = "Désorientations"
-	L["Mesmerizes (short)"] = "Désorientations (courtes)"
-	L["Fears"] = "Peurs"
-	L["Horrors"] = "Horreurs"
-	L["Mind Control"] = "Contrôle mental"
-	L["Stuns (short)"] = "Etourdissements (courts)"
-	L["Silences"] = "Silences"
-	L["Taunts"] = "Provocations"
-	L["Roots (short)"] = "Immobilisations (courtes)"
-	L["Knockbacks"] = "Projections"
+local locale = GetLocale()
+if locale == "deDE" then
+	--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "esES" then
+	--@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "esMX" then
+	--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "frFR" then
+	--@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "itIT" then
+	--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "koKR" then
+	--@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "ptBR" then
+	--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "ruRU" then
+	--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "zhCN" then
+	--@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
+elseif locale == "zhTW" then
+	--@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore")@
 end
 
 -- How long before DR resets
